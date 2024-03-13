23954e4331280bb02c624c936aae840c58c3348b
[username] <[user>
2024-03-13 23:47:23 +0700

    13/03/2024

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { ModalProvider } from "@/components/providers/modal-provider";
+import { SocketProvider } from "@/components/providers/socket-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -27,8 +28,10 @@ export default function RootLayout({
             enableSystem={false}
             storageKey="discord-theme"
           >
-            <ModalProvider/>
-            {children}
+            <SocketProvider>
+              <ModalProvider/>
+              {children}
+            </SocketProvider>
           </ThemeProvider>
         </body>
       </html>
---
+++ lib/current-profile-pages.ts
@@ -0,0 +1,20 @@
+import { getAuth } from "@clerk/nextjs/server";
+import { db } from "@/lib/db";
+import { NextApiRequest } from "next";
+
+export const currentProfilePages = async ( reg: NextApiRequest) => {
+    const { userId } = getAuth(reg);
+
+    if(!userId) {
+        return null;
+    }
+
+    const profile = await db.profile.
+    findUnique({
+        where: {
+            userId
+        }
+    });
+
+    return profile;
+}
